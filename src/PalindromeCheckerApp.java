9580c98eb6a1c209d7e4321c7be684df0fbbde57
Rishi <rishihansaliya07@gmail.com>
2026-03-05 20:04:41 +0530

    UC9: Refactor palindrome logic into a reusable method

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,24 +4,31 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("--- Advanced Palindrome Checker (reverse() Method) ---");
+        System.out.println("--- Refactored Palindrome Checker (Methods) ---");
         System.out.print("Enter text: ");
 
         String input = scanner.nextLine();
 
 
-        String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-
-        String reversed = new StringBuilder(cleaned).reverse().toString();
-
-
-        if (!cleaned.isEmpty() && cleaned.equals(reversed)) {
+        if (isPalindrome(input)) {
             System.out.println("Success: '" + input + "' is a palindrome.");
         } else {
             System.out.println("Result: '" + input + "' is NOT a palindrome.");
         }
 
         scanner.close();
+    }
+
+
+    public static boolean isPalindrome(String text) {
+        if (text == null || text.isEmpty()) {
+            return false;
+        }
+
+
+        String cleaned = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        return cleaned.equals(reversed);
     }
 }
